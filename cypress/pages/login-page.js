88b41c3dd7fe1dd5113742929d7ea876d5a3d8d0
Cyberspace27 <ariel.villarreal26@gmail.com>
2024-10-22 14:10:02 -0600

    Clean and code documented

--- cypress/pages/login-page.js
+++ cypress/pages/login-page.js
@@ -4,20 +4,34 @@ class loginPage{
     btnLogin = "button[type='submit'].orangehrm-login-button";
     lblDashboardMsg = "h6.oxd-topbar-header-breadcrumb-module";
 
+    /**
+     * Enters the given username into the username input field.
+     * @param user the username to enter
+     */
      setUserName(user) {
         cy.get(this.txtUserName).type(user);
         
     }
+/**
+ * Enters the given password into the password input field.
+ * @param pass the password to enter
+ */
     setUserPassword(pass) {
         cy.get(this.txtUserPass).type(pass);
         
     }
+/**
+ * Clicks the login button on the page.
+ */
     clickOnLogin() {
         cy.get(this.btnLogin).click();
         
     }
+/**
+ * Validates the dashboard message displayed after a successful login.
+ */
     validateDasboardMsg() {
-        
+       //need to be implemented 
     }
 }
 export default loginPage;
