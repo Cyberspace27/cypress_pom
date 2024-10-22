baa11bad17e26dc080d3f876b0778fa84b905719
Cyberspace27 <ariel.villarreal26@gmail.com>
2024-10-22 14:14:02 -0600

    Clean and code documented static class

--- cypress/pages/login-page-static.js
+++ cypress/pages/login-page-static.js
@@ -2,18 +2,32 @@ export class loginPageB{
    static txtUserName = "input[name='username']";
    static txtUserPass = "input[placeholder='Password']";
  
+/**
+ * Sets the username input field with the provided parameters.
+ * @param params the parameters to type into the username input field
+ */
      static setUserName(params) {
          cy.get(this.txtUserName).type(params);
          
      }
+    /**
+     * Sets the password input field with the provided parameters.
+     * @param params the parameters to type into the password input field
+     */
      static setUserPassword(params) {
          cy.get(this.txtUserPass).type(params);
          
      }
+    /**
+     * Clicks the login button on the page.
+     */
      static clickOnLogin() {
          cy.get().click();
          
      }
+    /**
+     * Verifies that the dashboard message is displayed after successful login.
+     */
      static validateDasboardMsg() {
          
      }
